fc2980b0228f66d5d005410a211a27c123e10884
gautamswami <gautams5974@gmail.com>
2023-05-23 16:50:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+// const io = require("socket.io")(8900,{
+//     cors:{
+//         origin:"*"
+//     }
+// });
+
+// io.on("connection",(socket)=>{
+//     console.log("A USER CONNECCTD")
+// })
+const io = require("socket.io")(8900, {
+    cors: {
+      origin: "*",
+    },
+  });
+  
+  let users = [];
+  
+  const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) &&
+      users.push({ userId, socketId });
+  };
+  
+  const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+  };
+  
+  const getUser = (userId) => { 
+    console.log(userId,'user id to find in',users)
+    return users.find((user) => user.userId === userId);
+  };
+  
+  io.on("connection", (socket) => {
+    //when ceonnect
+  
+    //take userId and socketId from user
+    socket.on("addUser", (userId) => {
+      addUser(userId, socket.id);
+      io.emit("getUsers", users);
+    });
+  
+    //send and get message
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+      const user = getUser(receiverId);
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+        receiverId
+      });
+    });
+  
+    //when disconnect
+    socket.on("disconnect", () => {
+      removeUser(socket.id);
+      io.emit("getUsers", users);
+    });
+  });
